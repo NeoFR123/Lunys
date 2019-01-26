e1ab64da190b05f6768ee68a13937ba65b6304b9
NeoFR123 <46282675+NeoFR123@users.noreply.github.com>
2019-01-26 21:54:04 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,1 +1,45 @@
+const Discord = require('discord.js');
+const Client = new Discord.Client();
 
+const prefix = ";"
+
+module.exports.help = {
+  name: "clear"
+}
+
+Client.on("ready", () => {
+	console.log("online");
+	Client.user.setPresence({ game: { name: `Fortnite 2.0`, type: 0} });
+});
+
+
+Client.on("message", async (message) => {
+	if (message.author.bot) return;
+	if (!message.content.startsWith(prefix)) return;
+	
+	let command = message.content.split(" ")[0];
+	command = command.slice(prefix.length);
+	
+	let args = message.content.split(" ").slice(1);
+
+	if (command === "say") {
+	if (message.member.hasPermission("MANAGE_MESSAGES"))
+		message.delete()
+        const embed = new Discord.RichEmbed()
+		.setColor(0x954D23)
+		.setDescription(args.join(" "));
+		message.channel.send({embed})
+	} else
+
+	if (command == "aide") {
+		const embed = new Discord.RichEmbed()
+		.setColor(0x954D23)
+		.setTitle("List des commandes:")
+		.addField("/OpTium", "Tu veux des réseaux sociaux ?")
+		.addField("/classement", "Tu veux le classement ?")
+		message.channel.send({embed})
+	}
+
+});
+
+Client.login(process.env.TOKEN);
